e6b7ad02ce22310741f16622244053280308711a
yasminkandil <97449378+yasminkandil@users.noreply.github.com>
2022-04-04 13:23:49 +0200

    Update view_users.php

--- app/views/pages/view_users.php
+++ app/views/pages/view_users.php
@@ -6,6 +6,7 @@
 </style>
 <?php
 
+
 class view_users extends View
 {
 
@@ -16,64 +17,32 @@ class view_users extends View
 
     require APPROOT . '/views/inc/header.php';
     $text = <<<EOT
-    <div class="jumbotron jumbotron-fluid">
+    <div class="bg-warning">
     <div class="container">
       <h1 class="display-4"><center> $title</center></h1>
     </div>
   </div>
-  <div class="container,table">
-	<table class="table table-striped table-bordered table-hover ">
-		<thead><tr class="table-warning">
+ 
+EOT;
+    echo $text;
+    $user=$this->model->getAllUsers();
+    $str="<table class='table table-striped table-bordered table-hover '>
+		<thead><tr class='table-warning'>
 			<th>ID</th>
 			<th>First Name</th>
 			<th>Last Name</th>
-			<th>Address</th>
 			<th>Email</th>
+			<th>Address</th>
 			<th>Phone Number</th>
 		
 		</tr>
-	</thead>
-	<tbody>
-		
-
-
-  <tr >
-    <td> 1</td>
-    <td>Yasmin</td>
-    <td>Kandil</td>
-    <td>new cairo</td>
-    <td>kandil@gmail.com</td>
-    <td>0100000000</td>
-  </tr>	
-  <tr>
-  <td> 2</td>
-    <td>Salma</td>
-    <td>Kandil</td>
-    <td>new cairo</td>
-    <td>kandil@gmail.com</td>
-    <td>0100000000</td>
-    </tr>
-    <tr>
-    <td> 3</td>
-      <td>Rogina</td>
-      <td>Kandil</td>
-      <td>new cairo</td>
-      <td>kandil@gmail.com</td>
-      <td>0100000000</td>
-      </tr>
-      <tr>
-    <td> 4</td>
-      <td>Miriam</td>
-      <td>Kandil</td>
-      <td>new cairo</td>
-      <td>kandil@gmail.com</td>
-      <td>0100000000</td>
-      </tr>
-  
-	</tbody>
-</table>
-EOT;
-    echo $text;
-    require APPROOT . '/views/inc/footer.php';
+    </thead>";
+        foreach($user as $x)
+          $str.="<tr><td>".$x->ID."</td><td>".$x->FirstName."</td><td>".$x->LastName."</td><td>".$x->Email."</td><td>".$x->Address."</td><td>".$x->Mobile."</td></tr>";
+      //var_dump($Ent[0]->Name);	
+    $str.="</table>";
+      echo $str;
+      require APPROOT . '/views/inc/footer.php';
   }
+
 }
