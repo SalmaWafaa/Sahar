390441a4c778d69a8a97159e6d0dfeaacd4365a0
miriamayman <miriam.ayman99@gmail.com>
2022-05-11 19:42:05 +0200

    back about us

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -9,27 +9,35 @@ class About extends view
     
 
     require APPROOT . '/views/inc/header.php';
+    $about=$this->model->About();
+    foreach($about as $abt)
+    
+   
     ?>
     
     <div class="p-3 mb-2 bg-warning bg-gradient text-dark">
     <div class="container">
       <h1 class="display-4"> About Us </h1>
+     
     </div>
   </div>
+  
  
   <div class="jumbotron jumbotron-fluid">  
   <div class="container">
       <h1 class="display-4"  > WHO WE ARE</h1>
-      <h2>We Are Talented Skilled Engineers with Great Experience of Mobile Phone Products and Service Since 1998.</h2>
-      <p>After Many Successful Stories in Cell Phone Field, We Decide to Establish SA7-R Company Which Provide Customers with Extensive Product and Professional Service to Their Mobile.</p>
-    </div>
+      <?php
+     echo $abt->whoWeAre
+      ?>
+      </div>
     </div>
 
     <div class="jumbotron jumbotron-fluid">  
     <div class="container">
         <h1 class="display-4"> Mission</h1>
-        <h2>Win Every Customer Trust..</h2>
-        <p>SA7-R Mission Is To Offer Its Customers The Highest Quality Cell Phone Products And Services. Its Owner Focuses On Personalized Service To His Customers By Offering Convenience And Rapid Service. Additionally, SA7-R Has The Technological Expertise To Assist Customers In Picking The Product And Service That Best Meets Their  Needs. Finally, Our Staff Will Have Strong Vendor Relationships With The Product Suppliers And Will Be Able To Meet Customers’ Demand For The Newest Innovation In Cellular Phone Technology.</p>
+        <?php
+     echo $abt->Mission
+      ?>
         <img src="<?php echo ImageRoot . 'mission.jpg' ; ?>" class="d-block w-100" alt="...">
       </div>
       </div>
