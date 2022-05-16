5a0df7ffef73d303e74b58e16a61a8561dcb6961
r0gina <rogina.michelle1@gmail.com>
2022-05-17 01:00:36 +0200

    b

--- app/models/add_productModel.php
+++ app/models/add_productModel.php
@@ -4,7 +4,7 @@ class add_productModel extends shopModel{
     public function add_product()
     {
         $this->dbh->query("INSERT INTO products (`ProductImage`,`ProductName`, `Description`, `Quantity`,`Price`) VALUES(:prod_img, :prod_name, :descr, :quantity, :price)");
-        $this->dbh->bind(':prod_img', $this->img);
+        $this->dbh->bind(':prod_img', $this->img1);
         $this->dbh->bind(':prod_name', $this->productName);
         $this->dbh->bind(':descr', $this->description);
         $this->dbh->bind(':quantity', $this->quantity);
