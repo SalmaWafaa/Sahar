7147f3b1052920291691fb07a38e2d3140aaa70c
miriamayman <miriam.ayman99@gmail.com>
2022-05-20 01:02:39 +0200

    contact model

--- app/models/ContactModel.php
+++ app/models/ContactModel.php
@@ -1,6 +1,76 @@
 <?php
 class ContactModel extends model{
 	public $title='Contact Page';
+	protected $dsent;
+	protected $userMail;
+	protected $userID;
+	protected $Subj;
+	protected $msg;
+	public function __construct()
+    {
+        parent::__construct();
+        $this->dsent     = "";
+        $this->userMail = "";
+        $this->userID     = "";
+        $this->Subj = "";
+		$this->msg = "";
+	}
+	public function getdsent()
+    {
+        return $this->dsent;
+    }
+	public function getuserMail()
+    {
+        return $this->userMail;
+    }
+
+	public function getuserID ()
+    {
+        return $this->userID ;
+    }
+
+	public function getSubj()
+    {
+        return $this->Subj;
+    }
+
+	public function getmsg()
+    {
+        return $this->msg;
+    }
+	public function setdsent($dsent)
+    {
+        $this->dsent = $dsent;
+    }
+	public function setuserMail($userMail)
+    {
+        $this->userMail = $userMail;
+    }
+	public function setuserID($userID)
+    {
+        $this->userID = $userID;
+    }
+	public function setSubj($Subj)
+    {
+        $this->Subj = $Subj;
+    }
+	public function setmsg($msgt)
+    {
+        $this->msg= $msg;
+    }
+
+    public function contactMsg()
+    {
+        $this->dbh->query("INSERT INTO contacttable (`datSent`,`UserEmail`, `UserID`, `Subject`,`Message`) VALUES(:dsent, :userMail, :userID,  :Subj, :msg)");
+        $this->dbh->bind(':dsent', $this->dsent);
+        $this->dbh->bind(':userMail', $this->userMail);
+        $this->dbh->bind(':userID', $this->userID);
+        $this->dbh->bind(':Subj', $this->Subj);
+        $this->dbh->bind(':msg', $this->msg); 
+
+        return $this->dbh->execute();
+    }
+
 	public function Contact()
     {
         $this->dbh->query('SELECT * from contactdata ');
@@ -8,5 +78,6 @@ class ContactModel extends model{
         return $records;
 
     }
+	
 	
 }
