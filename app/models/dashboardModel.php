039f6e4241cee9573b366122163c8b5068916b6d
miriamayman <miriam.ayman99@gmail.com>
2022-05-22 22:09:35 +0200

    dash1

--- app/models/dashboardModel.php
+++ app/models/dashboardModel.php
@@ -1,5 +1,11 @@
 <?php
 class dashboardModel extends model{
 	public $title='Dashboard Page';
-	
+	public function Dash()
+    {
+        $this->dbh->query('SELECT * from contacttable ');
+        $records=$this->dbh->resultSet();
+        return $records;
+
+    }
 }
