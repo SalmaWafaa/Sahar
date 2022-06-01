2b579039d1fc2aac39dae58ff0312d5e5f63806f
r0gina <rogina.michelle1@gmail.com>
2022-06-01 13:36:00 +0200

    ajax

--- app/models/searchModel.php
+++ app/models/searchModel.php
@@ -29,6 +29,48 @@ if (mysqli_num_rows($result) > 0) {
     echo          "<p class='card-text'>" . $row['email'] . "</p>";
    
     echo      "</div>";
+
+    
+       ?>
+       <div class="row row-cols-2 row-cols-md-4">
+         <?php
+   
+  <div class="col mb-4">
+      <form action="" method="post">
+    <div class="card shadow">
+    <a href="<?php echo URLROOT . "products/product?id=$p->ProductID&cid=$p->Cat_ID";?>">
+      <img src="<?php echo ImageRoot . $p->ProductImage ; ?>" alt="Image1" class="card-img-top"  >
+</div>
+   </a>
+   
+      <div class="card-body">
+        <h5 class="card-title"><?php echo $p->ProductName;?></h5>
+        <h6>
+                              <i class="bi bi-star-fill"></i>
+                              <i class="bi bi-star-fill"></i>
+                              <i class="bi bi-star-fill"></i>
+                              <i class="bi bi-star-fill"></i>
+                              <i class="bi bi-star-fill"></i>
+        </h6>
+        <p class="card-text"> <?php echo $p->Description ;?>
+                              </p>
+                              <h5>
+                              <small><s class="text-secondary">419 EGP</s></small>
+                               <span class="price"> <?php echo $p->Price;?> EGP</span>
+                              </h5>
+                              <button class="btn btn-warning my-3" name="add">Add to Cart <i class="bi bi-cart4"></i><a href="<?php echo URLROOT . 'products/cart?pid='.$p->ProductID; ?>" ></a></button>
+                               <input type='hidden' name='product_id' value='<?php  $p->ProductID;?>'>
+      </div>
+</form>
+   </div>
+   
+  <?php 
+  
+
+  ?>
+  </div>
+
+
   }
   echo "</div>";
 } else {
