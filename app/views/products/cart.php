ad4031b8ff3a82d0e3c780d8c7c968377e501f41
yasminkandil <yasminkandil2001@gmail.com>
2022-06-02 22:02:21 +0200

    bb

--- app/views/products/cart.php
+++ app/views/products/cart.php
@@ -2,15 +2,23 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cart</title>
+
+    <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
     <!-- Bootstrap CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <link rel="stylesheet" href="style.css">
-  
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
 </head>
 <body class="bg-light">
     
@@ -18,104 +26,84 @@
 <?php
 class cart extends View
 {
-  public function output()
-    {
-        $cart= $this->model;
-       // $product= $this->model->getProduct($_GET["pid"]);
-     $title = $this->model->title;
-      require APPROOT . '/views/inc/header.php';
-      if(!empty($_POST['cart'])) 
-     {
-		$this->model->productsQuantity=json_decode($_POST['cart'],true);?>
-        <button type="button" class="btn btn-danger">Your cart is currently empty!</button>
-<?php
-     }
-     if(!empty($_GET["action"])) 
-     {
-	  switch($_GET["action"]) 
-      {
-		case "add":
-			if(!empty($_POST["quantity"])) {
-				$this->model->addProduct($_GET["pid"],$_POST["quantity"]);
-			}
-		break;
-		case "remove":
-			$this->model->removeProduct($_GET["pid"]);
-		break;
-		case "empty":
-			$this->model->emptyCart();	
-		break;	
-	   }
-      if(count($cart->productsQuantity)>0)
-      {
-		$item_total = 0;
-        ?>
-        <button type="button" class="btn btn-danger">Your cart is currently empty!</button>
-        <?php
-      }
-     }
-  
- 
-      ?>
-      <div class="container">
-     <div class="bg-warning">
-     <h1 class="display-4"> <center> My Cart</center></h1>
-     </div>
-     <div class=”Cart-Container”></div>
-     <div class=”Header”>
-     <h3 class=”Heading”><i class="bi bi-cart3"></i> Preview Your Cart </h3>
-      <button type="button" class="btn btn-info"><a href="<?php echo URLROOT . 'products/shop'; ?>">Continue Shopping</a> </button>
-     <button type="button" class="btn btn-warning"><a href="<?php echo URLROOT . 'products/Checkout'; ?>">Proceed to checkout</a></button>
-    </div>
-               <table class='table table-striped table-bordered table-hover '>
-		     <thead><tr class='table-warning'>
-             <tr>
-				<th><strong>Name</strong></th>
-                <th><strong>Description</strong></th>
-				<th><strong>Quantity</strong></th>
-				<th><strong>Price</strong></th>
-                <th><strong>Rate</strong></th>
-				<th><strong>Action</strong></th>
-			 </tr></thead>
-			 <?php	
-             $item_total=0;
-			 foreach ($cart->productsQuantity as $productID => $Quantity)
-               {  
-				 $product=$this->model->prod($productID);						
-				 ?>
-				 <tr>
-					<td><strong><?php echo $product->ProductName; ?></strong></td>
-                    <td><strong><?php echo $product->Description; ?></strong></td>
-					<td><?php echo $Quantity; ?></td>
-					<td><?php echo "EPG".$product->Price; ?></td>
-                    <td><strong><?php echo $product->Rate; ?></strong></td>
-					<td>
-						<form method="post" action= "<?php URLROOT . 'products/cart?action=remove&id='.$product->ProductID; ?>">
-							<input type="submit" value="Remove Item" class="btnAddAction" />
-							<input type='hidden' name='cart' value='<?php echo (json_encode($cart->productsQuantity)); ?>' />
-						</form>
-					</td>
-				 </tr>
-				 <?php
-				 $item_total += ($product->Price*$Quantity);
-			   }
-			 ?>
-			 <tr>
-				<td colspan="4"><strong>Total:</strong> 
-				<?php 
-				echo "EPG".$item_total; ?></td>
-			   </tr>
-		      </table>		
-	           <?php
-           
-    
-     ?>
-      </div>
-     </div>
-     <?php
-     
-      require APPROOT . '/views/inc/footer.php'; 
+    function cartElement($productimg, $productname, $productprice, $productid){
+        $element = "
+        
+        <form action=\"cart.php?action=remove&id=$productid\" method=\"post\" class=\"cart-items\">
+                        <div class=\"border rounded\">
+                            <div class=\"row bg-white\">
+                                <div class=\"col-md-3 pl-0\">
+                                    <img src=$productimg alt=\"Image1\" class=\"img-fluid\">
+                                </div>
+                                <div class=\"col-md-6\">
+                                    <h5 class=\"pt-2\">$productname</h5>
+                                    <small class=\"text-secondary\">Seller: dailytuition</small>
+                                    <h5 class=\"pt-2\">$$productprice</h5>
+                                    <button type=\"submit\" class=\"btn btn-danger mx-2\" name=\"remove\">Remove</button>
+                                </div>
+                                <div class=\"col-md-3 py-5\">
+                                    <div>
+                                        <button type=\"button\" class=\"btn bg-light border rounded-circle\"><i class=\"fas fa-minus\"></i></button>
+                                        <input type=\"text\" value=\"1\" class=\"form-control w-25 d-inline\">
+                                        <button type=\"button\" class=\"btn bg-light border rounded-circle\"><i class=\"fas fa-plus\"></i></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+        
+        ";
+        echo  $element;
     }
-   
-}?>
+  public function output()
+  { 
+      
+    $title = $this->model->title;
 
+    //$title = $this->model->title;
+
+
+    require APPROOT . '/views/inc/header.php';
+    
+    
+    ?>
+    
+    
+
+<body>
+<?php
+ $PN=$this->model->getProductCartName($_SESSION['user_id']);
+ $userID=$_SESSION['user_id'];
+ for($i=0;$i<count($PN);$i++){
+?>
+    <form action="cart.php?action=remove&id=$productid" method="post" class="cart-items">
+    <div class="border rounded">
+        <div class="row bg-white">
+            <div class="col-md-3 pl-0">
+                <img src=<?php  echo ImageRoot . $this->model->getProductCartImage($_SESSION['user_id'])[$i];?> alt="Image1" class="img-fluid">
+            </div>
+            <div class="col-md-6">
+                <h5 class="pt-2"><?php echo $this->model->getProductCartName($_SESSION['user_id'])[$i];?></h5>
+                <h5 class="pt-2"><?php echo $this->model->getProductCartPrice($_SESSION['user_id'])[$i];?></h5>
+                <h5 class="pt-2"><?php echo $this->model->getProductCartColor($_SESSION['user_id'])[$i];?></h5>
+                <h5 class="pt-2"><?php echo $this->model->getProductCartQuality($_SESSION['user_id'])[$i];?></h5>
+                <button type="submit" class="btn btn-danger mx-2" name="remove">Remove</button>
+            </div>
+            <div class="col-md-3 py-5">
+                <div>
+                    <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-minus"></i></button>
+                    <input type="text" value="<?php echo $this->model->getProductCartQuantity($_SESSION['user_id'])[$i];?>" class="form-control w-25 d-inline">
+                    <button type="button" class="btn bg-light border rounded-circle"><i class="fas fa-plus"></i></button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+
+<?php
+ }
+    require APPROOT . '/views/inc/footer.php';
+    //require APPROOT . '/views/inc/footer.php';
+  }
+}
+?>
