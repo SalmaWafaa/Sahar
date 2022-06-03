b030b2bf5079d68cf4b44cb4d0a90559f9154e20
SalmWwafaa <salma1905060@miuegypt.edu.eg>
2022-06-03 16:35:35 +0200

    Order History

--- app/models/OrdershistoryModel.php
+++ app/models/OrdershistoryModel.php
@@ -4,12 +4,19 @@ class OrdershistoryModel extends model
     public  $title = 'Order History';
     public function ordersHistory($id)
     {
-    $this->dbh->query('SELECT * from orders,products WHERE `ID` =:id');
+      
+
+   $this->dbh->query(' SELECT * FROM products,orders,users,cart WHERE orders.ID = users.ID AND users.ID = cart.User_ID AND products.ProductID=cart.Product_ID
+   GROUP BY :id');
+
+
+
+
     $this->dbh->bind(':id',$id);
     $Record = $this->dbh->resultSet();
     return $Record;
     }
-    
+ 
 
 }
 ?>
